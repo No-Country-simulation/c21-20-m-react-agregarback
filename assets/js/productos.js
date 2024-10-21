fb655237972a5029ebc01b6a13ff96a89cc3fe15
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-10-21 11:54:37 -0300

    Agregué un dropdown

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,3 +1,11 @@
-document.getElementById("menuButton").addEventListener("click", () => {
-    document.getElementById("navbar").classList.toggle("active")
+document.getElementById("filterBtn").addEventListener("click", () => {
+    document.getElementById("filterShadow").classList.add("active")
+    document.getElementById("filter").classList.add("active")
+})
+
+document.getElementById("filterShadow").addEventListener("click", (e) => {
+    if (e.target.id == "filterShadow") {
+        document.getElementById("filterShadow").classList.remove("active")
+    document.getElementById("filter").classList.remove("active")
+    }
 })
